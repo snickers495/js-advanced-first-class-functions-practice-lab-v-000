1155ec407d81436c0ea503a4bd1e8bcdb82c5e81
Lisa Lee <snickers495@gmail.com>
2018-01-14 17:36:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function driversByName(arr){
   })
 }
 function totalRevenue(arr){
-  let newArr = arr.reduce(function(total, el, i, ele2) {
+  let newArr = arr.reduce(function(total, ele2) {
     return (total + ele2.revenue) }, 0);
 }
 function averageRevenue(arr) {
