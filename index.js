f537231d30f9b57c0f5474b26c279d60fad373c8
Lisa Lee <snickers495@gmail.com>
2018-01-14 17:34:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+ function logDriverNames(arr) {
+   arr.forEach(name => console.log(name.name));
+ }
+ function logDriversByHometown(arr, word) {
+   arr.forEach(function(name) {
+     if (name.hometown === word) {
+       console.log(name.name);
+     }
+   })
+ }
+ function driversByRevenue(arr) {
+   const newArr = arr.slice();
+   return newArr.sort(function(ele, ele2) {
+     return ele.revenue - ele2.revenue;
+   });
+ }
+function driversByName(arr){
+  const newArr = arr.slice();
+  return newArr.sort(function(ele, ele2) {
+    return (ele.name).localeCompare(ele2.name);
+  })
+}
+function totalRevenue(arr){
+  let newArr = arr.reduce(function(total, ele2) {
+    return (total + ele2.revenue) });
+}
+function averageRevenue(arr) {
+  let newRev = arr.reduce((total, curr) => total + curr.revenue
+  )
+  return newRev
+}
