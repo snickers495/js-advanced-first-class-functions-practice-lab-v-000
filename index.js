8fe77999bfa22f96d008ea498c681803fb07a716
Lisa Lee <snickers495@gmail.com>
2018-01-14 17:37:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+ function logDriverNames(arr) {
+   arr.forEach(name => console.log(name.name));
+ }
+ function logDriversByHometown(arr, word) {
+   arr.forEach(function(name) {
+     if (name.hometown === word) {
+       console.log(name.name);
+     }
+   })
+ }
+ function driversByRevenue(arr) {
+   const newArr = arr.slice();
+   return newArr.sort(function(ele, ele2) {
+     return ele.revenue - ele2.revenue;
+   });
+ }
+function driversByName(arr){
+  const newArr = arr.slice();
+  return newArr.sort(function(ele, ele2) {
+    return (ele.name).localeCompare(ele2.name);
+  })
+}
+function totalRevenue(arr){
+  return arr.reduce(function(total, ele2) {
+    return (total + ele2.revenue) }, 0);
+}
+function averageRevenue(arr) {
+  return totalRevenue(arr) / arr.length
+}
